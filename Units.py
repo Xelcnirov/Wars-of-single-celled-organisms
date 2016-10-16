78668496200759865396d0569225c803b54d2404
White Lich <emrakul@yandex.ru>
2016-10-16 17:01:42 +0300

    Enemy file

--- Units.py
+++ Units.py
@@ -1,57 +1,42 @@
-from random import choice
-
-
 class Unit:
-    def __init__(self, x, y, r=50, shooting_range=150, speed=10):
+    def __init__(self, x, y, shooting_range=150, r=50):
         self.x = x
         self.y = y
         self.r = r
         self.id = str(id(self))
-        self.group = self.__class__.__name__
-        self.shooting_range = shooting_range  # may be subclass attribute
-        self.speed = speed
+        self.shooting_range = shooting_range #may be subclass attribute
 
-    def move_up(self):
-        self.y -= self.speed
-
-    def move_down(self):
-        self.y += self.speed
-
-    def move_left(self):
-        self.x -= self.speed
-
-    def move_right(self):
-        self.x += self.speed
-
-    def collide(self, another_object):  # need World's collide func
-        self.move_away(another_object.x, another_object.y)
-
-    def tick(self):  # frame actions here
-        choice([self.move_down, self.move_up, self.move_left, self.move_right])()
-
-    def kill(self):  # Object destruction function
-        # canvas.delete(self)
+    def colliding(self, another_object): #need World's collide func
         pass
+
+
+    def tick(self): #frame actions here
+        pass
+
+    def kill(self): #Object destruction function
+        canvas.delete(self)
 
     def move_to(self, ax, ay):
         if self.x > ax:
-            self.move_left()
+            self.x -= 1
         elif self.x < ax:
-            self.move_right()
+            self.x += 1
         if self.y > ay:
-            self.move_up()
+            self.y -= 1
         elif self.y < ay:
-            self.move_down()
+            self.y += 1
 
     def move_away(self, ax, ay):
         if self.x > ax:
-            self.move_right()
+            self.x += 1
         elif self.x < ax:
-            self.move_left()
+            self.x -= 1
         if self.y > ay:
-            self.move_down()
+            self.y += 1
         elif self.y < ay:
-            self.move_up()
+            self.y -= 1
 
-    def shoot(self):  # Doesn't need args, always shoots at hero
+    def shoot(self): #Doesnt need args, always shoots at hero
         pass
+
+
